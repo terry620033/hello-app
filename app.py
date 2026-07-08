dd4213f81f5ec836b55ebfe182275f3c5daa7d54
Terry Tse <terry620033@gmail.com>
2026-07-08 00:22:51 -0400

    Update version in hello message to 2.18

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Hello from Kubernetes! K8s Cluster is Powered by CM4, PiTray & TuringPi. Version 2.8</h1>'
+    return '<h1>Hello from Kubernetes! K8s Cluster is Powered by CM4, PiTray & TuringPi. Version 2.18</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
